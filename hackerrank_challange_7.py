765a52810f300978cf3dcd0b337faa3dc6d0b3f3
Chandan Soni <90852214+Chandansoni2021@users.noreply.github.com>
2022-06-24 10:10:48 +0530

    string_pangram

---
+++ hackerrank_challange_7.py
@@ -0,0 +1,18 @@
+s=input()
+data=[]
+f=[]
+c=0
+count=0
+for i in s:
+    data.append(i.lower())
+for i in range(97,123):
+    f.append(chr(i))
+for i in f:
+    if i==' ':
+        len(s)-1
+    if celse:
+        c=1
+if c>0:
+    print("pangram")
+else:
+    print("not pangram")
